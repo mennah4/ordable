be8cccc5ec27d4c672050be4342486476ddedd63
mennah4 <mennahjafar@hotmail.com>
2024-03-17 19:32:54 +0300

    Update nav components

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -6,37 +6,66 @@ import CartModal from "../modules/customer/pages/CartModal";
 import { useAppSelector } from "../redux";
 import { CUSTOMER_STORE_NAME } from "../redux/constants";
 import LoginModal from "../modules/customer/pages/LoginModal";
+import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useLocation } from 'react-router-dom';
+import { logoutUser } from "../modules/user/userActions";
 
 export default function NavBar() {
     const { isOpen, onOpen, onClose } = useDisclosure();
-    const loginModal = useDisclosure();
+    const navigate = useNavigate();
+    const location = useLocation();
+    const customer = useAppSelector((state) => state[CUSTOMER_STORE_NAME].customer)
     const cart = useAppSelector((state) => state[CUSTOMER_STORE_NAME].cart);
+    const storeInfo = {
+        name: process.env.REACT_APP_STORE_NAME,
+        id: process.env.REACT_APP_STORE_ID
+    };
 
+    useEffect(() => {
+        console.log("customer in navbar", customer)
+    }, [customer])
 
     return (
         <Navbar isBordered className="">
             <NavbarBrand>
-                <p className="font-bold text-inherit">ORDABLE/</p>
+                <a href="/products" className="font-bold text-blue-600 text-inherit">{storeInfo.name}</a>
             </NavbarBrand>
-            <NavbarItem>
-                <Search />
-            </NavbarItem>
             <NavbarContent justify="end">
-                <NavbarItem className="hidden lg:flex">
-                    <LoginModal mode="login" />
-                </NavbarItem>
-                <NavbarItem>
-                    <LoginModal mode="signup" />
-                </NavbarItem>
-                <div onClick={onOpen} className="relative flex cursor-pointer h-11 w-11 items-center justify-center rounded-md border border-neutral-200 text-black transition-colors dark:border-neutral-700 dark:text-white">
-                    <ShoppingCartIcon />
+                {!customer &&
+                    <>
+                        <NavbarItem className="hidden lg:flex">
+                            <LoginModal mode="login" />
+                        </NavbarItem>
+                        <NavbarItem>
+                            <LoginModal mode="signup" />
+                        </NavbarItem>
+                    </>}
 
-                    <div className="absolute right-0 top-0 -mr-2 -mt-2 h-4 w-4 rounded bg-blue-600 text-[11px] font-medium text-white">
-                        <p className="flex items-center justify-center">{cart?.cart_items?.length ?? 3}</p>
-                    </div>
-                </div>
+                {customer &&
+                    <>
+                        <Button onPress={() => navigate('/customer/orders')} color="primary" variant="bordered">
+                            My orders
+                        </Button>
+                        <Button onPress={() => {
+                            logoutUser('customer')
+                            navigate('/products');
+                        }} 
+                            color="primary" variant="bordered">
+                            Logout
+                        </Button>
+                        
+                        <div onClick={onOpen} className="relative flex cursor-pointer h-11 w-11 items-center justify-center rounded-md border border-neutral-200 text-black transition-colors dark:border-neutral-700 dark:text-white">
+
+                            <ShoppingCartIcon />
+
+                            <div className="absolute right-0 top-0 -mr-2 -mt-2 h-4 w-4 rounded bg-blue-600 text-[11px] font-medium text-white">
+                                <p className="flex items-center justify-center">{cart?.cart_items?.length ?? 0}</p>
+                            </div>
+                        </div>
+                    </>}
             </NavbarContent>
-            <CartModal isOpen={isOpen} onClose={onClose} />
+            {customer && <CartModal isOpen={isOpen} onClose={onClose} />}
         </Navbar>
     );
 }
